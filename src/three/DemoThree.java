8a8f4ac1b68511b533893e8b8ecb1ffbcad7c005
oscar <oscar@192.168.2.7>
2021-11-24 19:04:16 -0600

    update

--- src/three/DemoThree.java
+++ src/three/DemoThree.java
@@ -4,9 +4,13 @@ public class DemoThree {
 	public static void main(String[] args) {
 		
 		System.out.println("This is my 3rd attempt");
+		
+		System.out.println("Test");
 	}
 	
 	public static void add(int a, int b) {
 		System.out.println( a + b);
 	}
+	
+
 }
